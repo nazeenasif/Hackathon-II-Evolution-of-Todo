f4bb6d63ad0c6088bd253b4a744aa0f9d365a8ed
Nazeen Asif <168020715+nazeenasif@users.noreply.github.com>
2026-01-29 13:13:20 +0500

    Delete Phase-II/backend directory

--- Phase-II/backend/src/api/v1/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.api.v1.endpoints import tasks, auth
-
-
-api_router = APIRouter()
-api_router.include_router(tasks.router, prefix="", tags=["tasks"])
-api_router.include_router(auth.router, prefix="", tags=["auth"])
